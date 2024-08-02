e68c6e853f3881df57350306e93653d849343b32
Steven Adams <steven.adams.r@gmail.com>
2024-08-02 09:15:08 -0400

    Chat window styling

--- apps/client/src/ui/components/Chat.svelte
+++ apps/client/src/ui/components/Chat.svelte
@@ -70,11 +70,11 @@
 </script>
 
 
-<div class='absolute left-0 bottom-0 w-96 bg-primary-700/50 p-2 backdrop-blur rounded space-y-2'>
-  <div bind:this={container} class='h-64 px-1 bg-primary-900 rounded flex-1 pointer-events-auto overflow-y-scroll'>
+<div class='group has-[:focus]:bg-primary-900/60 absolute left-0 bottom-0 w-96 p-2 rounded space-y-2'>
+  <div bind:this={container} class='group-has-[:focus]:bg-primary-900 h-48 px-1 rounded flex-1 pointer-events-auto overflow-y-hidden'>
     {#each messages as message}
       <p class='select-none'>{message}</p>
     {/each}
   </div>
-  <input bind:this={text} onfocus={handleFocus} onblur={handleBlur} class='bg-primary-900 p-1 rounded w-full' maxlength="48"/>
+  <input bind:this={text} onfocus={handleFocus} onblur={handleBlur} class='group-has-[:focus]:bg-primary-900 bg-transparent p-1 rounded w-full' maxlength="48"/>
 </div>
